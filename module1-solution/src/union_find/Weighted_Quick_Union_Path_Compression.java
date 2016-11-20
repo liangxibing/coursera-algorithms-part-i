f6ef4c791e7cdac52f5e19583a67d0887d3b926b
liangxibing <simonlaung@gmail.com>
2016-11-21 00:52:09 +1300

    my solution for Weighted Quick Union with Path Compression

---
+++ module1-solution/src/union_find/Weighted_Quick_Union_Path_Compression.java
@@ -0,0 +1,46 @@
+package union_find;
+
+/**
+ * Created by liangxibing on 20/11/16.
+ */
+public class Weighted_Quick_Union_Path_Compression {
+    private int[] array;
+    private int[] weight;
+
+    public Weighted_Quick_Union_Path_Compression(int n) {
+        array = new int[n];
+        for (int i : array)
+            array[i] = i;
+        weight = new int[n];
+        for (int i : weight)
+            weight[i] = 1;
+    }
+
+    public boolean connected(int p, int q) {
+        return findParent(p) == findParent(q);
+    }
+
+    public void union(int p, int q) {
+        int parentP = findParent(p);
+        int parentQ = findParent(q);
+        if (parentP != parentQ) {
+            if (weight[p] > weight[q]) {
+                array[parentQ] = parentP;
+                weight[p] += weight[q];
+            }
+            else {
+                array[parentP] = parentQ;
+                weight[q] += weight[p];
+            }
+        }
+    }
+
+    private int findParent(int i) {
+        int result = i;
+        while (array[result] != result) {
+            array[result] = array[array[result]];
+            result = array[result];
+        }
+        return result;
+    }
+}
